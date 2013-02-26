5ed77ea295ee8650dfb298da5d9f9ce91efc1e38
Alex Schroeder <alex@epfarms.org>
2013-02-26 11:57:51 -0500

    Get rid of stray DOCTYPE.

--- text-mapper.pl
+++ text-mapper.pl
@@ -164,8 +164,6 @@ sub svg {
      ($maxx) * 1.5 * $dx + $dx + 10, ($maxy + 1.5) * $dy + 10);
 
   my $doc = qq{<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
      viewBox="$minx $miny $maxx $maxy"
      xmlns:xlink="http://www.w3.org/1999/xlink">
