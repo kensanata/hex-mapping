9a7d841da107c7b2abc6046fc1f6a4b8cf6de0d7
Alex Schroeder <alex@gnu.org>
2016-08-06 00:26:20 +0200

    Swamps work in a way, but slow

--- text-mapper.pl
+++ text-mapper.pl
@@ -1040,8 +1040,8 @@ use Modern::Perl;
 # form "0105" and the value is whatever is the map description, so it can be a
 # number of types, plus a label, plus maybe a font size, etc.
 
-my $width = 20;
-my $height = 10;
+my $width = 10;
+my $height = 5;
 
 my $delta = [[[-1,  0], [ 0, -1], [+1,  0], [+1, +1], [ 0, +1], [-1, +1]],  # x is even
 	     [[-1, -1], [ 0, -1], [+1, -1], [+1,  0], [ 0, +1], [-1,  0]]]; # x is odd
@@ -1144,25 +1144,50 @@ sub lakes {
 sub swamps {
   # local minima larger than 1 are swamps
   my ($world, $altitude) = @_;
-  for my $current_altitude (0 .. 10) {
-  HEX:
-    for my $coordinates (keys %$altitude) {
-      # found a hex at the right altitude
-      if ($altitude->{$coordinates} == $current_altitude) {
-	# check the neighbors
-	for my $i (0 .. 5) {
-	  my ($x, $y) = neighbor($coordinates, $i);
-	  # ignore neighbors beyond the edge of the map
-	  next if $x <= 0 or $x > $width or $y <= 0 or $y > $height;
-	  my $other = sprintf("%02d%02d", $x, $y);
-	  # if the neighbor is at a lower altitude, skip it
-	  next HEX if $altitude->{$other} < $current_altitude;
-	}
-	# if no neighbor worth skipping was found, then this is a swamp
-	warn "$coordinates is a swamp\n";
+  my %drained;
+  my $drained;
+  do {
+    $drained = 0;
+    for my $coordinates (sort keys %$altitude) {
+      next if $drained{$coordinates};
+      warn "$coordinates:\n";
+      if ($world->{$coordinates} =~ /^lake/) {
+	$drained{$coordinates} = 1;
+      } elsif ($altitude->{$coordinates} <= 2) {
+	$drained{$coordinates} = 1;
 	$world->{$coordinates} = qq{swamp$altitude->{$coordinates} "swamp $altitude->{$coordinates}"};
       }
+      # check the neighbors
+      for my $i (0 .. 5) {
+	my ($x, $y) = neighbor($coordinates, $i);
+	# ignore neighbors beyond the edge of the map
+	next if $x <= 0 or $x > $width or $y <= 0 or $y > $height;
+	my $other = sprintf("%02d%02d", $x, $y);
+	# if the neighbor is at a lower altitude, we are drained
+	if ($drained{$coordinates} and $altitude->{$other} >= $altitude->{$coordinates}) {
+	  warn "$other drains to $coordinates because $coordinates is drained\n";
+	  $drained = 1 unless $drained{$other};
+	  $drained{$other} = 1;
+	} elsif ($drained{$other} and $altitude->{$other} <= $altitude->{$coordinates}) {
+	  warn "$coordinates drains to $other because $other is drained\n";
+	  $drained = 1 unless $drained{$coordinates};
+	  $drained{$coordinates} = 1;
+	} elsif ($altitude->{$other} < $altitude->{$coordinates}) {
+	  warn "$coordinates drains to $other because $other is lower\n";
+	  $drained = 1 unless $drained{$coordinates};
+	  $drained{$coordinates} = 1;
+	} elsif ($altitude->{$other} > $altitude->{$coordinates}) {
+	  warn "$other drains to $coordinates because $coordinates is lower\n";
+	  $drained = 1 unless $drained{$other};
+	  $drained{$other} = 1;
+	}
+      }
     }
+    warn "Drained $drained\n";
+  } while $drained;
+  for my $coordinates (keys %$altitude) {
+    next if $drained{$coordinates};
+    $world->{$coordinates} = qq{swamp$altitude->{$coordinates} "swamp $altitude->{$coordinates}"};
   }
 }
 
