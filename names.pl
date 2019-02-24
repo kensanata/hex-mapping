b90067142c6de148823fb6c7c95d8862e9a249a2
Alex Schroeder <alex@gnu.org>
2019-02-24 11:53:43 +0100

    names: fix commentary

--- names.pl
+++ names.pl
@@ -12,8 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Algorithms based on Traveller ©2008 Mongoose Publishing.
 
 use Mojolicious::Lite;
 use Modern::Perl;
