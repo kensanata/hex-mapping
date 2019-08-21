b62e36c289a3958479a8fa3b23c4b99e8b32479c
Alex Schroeder <alex@gnu.org>
2019-08-21 17:00:57 +0200

    text-mapper: fix help page order

--- text-mapper.pl
+++ text-mapper.pl
@@ -16,24 +16,6 @@
 package main;
 use Modern::Perl;
 
-=head2 Configuration
-
-As a Mojolicious application, it will read a config file called
-F<text-mapper.conf> in the same directory, if it exists. As the default log
-level is 'warn', one use of the config file is to change the log level using
-the C<loglevel> key.
-
-The libraries are loaded from the F<contrib> URL. You can change the default
-using the C<contrib> key. This is necessary when you want to develop locally,
-for example.
-
-    {
-      loglevel => 'debug',
-      contrib => 'file://contrib',
-    };
-
-=cut
-
 my $dx = 100;
 my $dy = 100*sqrt(3);
 my $debug;
@@ -2926,6 +2908,22 @@ L<$contrib/gridmapper.txt>
 
 Result:
 L<https://campaignwiki.org/text-mapper?map=include+$contrib/gridmapper-example.txt>
+
+=head2 Configuration
+
+As a Mojolicious application, it will read a config file called
+F<text-mapper.conf> in the same directory, if it exists. As the default log
+level is 'warn', one use of the config file is to change the log level using
+the C<loglevel> key.
+
+The libraries are loaded from the F<contrib> URL. You can change the default
+using the C<contrib> key. This is necessary when you want to develop locally,
+for example.
+
+    {
+      loglevel => 'debug',
+      contrib => 'file://contrib',
+    };
 
 =head2 Command Line
 
