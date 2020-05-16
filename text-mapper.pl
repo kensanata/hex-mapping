eb946178fa1e7682bd885a28e98db124256f7c31
Alex Schroeder <alex@gnu.org>
2020-05-17 00:37:03 +0200

    text-mapper: add archipelago code

--- text-mapper.pl
+++ text-mapper.pl
@@ -2257,7 +2257,6 @@ sub lakes {
   }
 }
 
-
 sub islands {
   my $self = shift;
   my ($world, $altitude) = @_;
@@ -2277,7 +2276,7 @@ sub islands {
 
 sub generate {
   my $self = shift;
-  my ($world, $altitude, $water, $rivers, $settlements, $trails, $canyons, $step) = @_;
+  my ($world, $altitude, $settlements, $trails, $step) = @_;
   # %flow indicates that there is actually a river in this hex
   my $flow = {};
 
@@ -2329,15 +2328,12 @@ sub generate_map {
   # keys for all hashes are coordinates such as "0101".
   # %world is the description with values such as "green forest".
   # %altitude is the altitude with values such as 3.
-  # %water is the preferred direction water would take with values such as 0
-  # (north west); 0 means we need to use "if defined".
-  # @rivers are the rivers with values such as ["0102", "0202"]
   # @settlements are are the locations of settlements such as "0101"
   # @trails are the trails connecting these with values as "0102-0202"
   # $step is how far we want map generation to go where 0 means all the way
-  my ($world, $altitude, $water, $rivers, $settlements, $trails, $canyons) =
-      ({}, {}, {}, [], [], [], []);
-  $self->generate($world, $altitude, $water, $rivers, $settlements, $trails, $canyons, $step);
+  my ($world, $altitude, $settlements, $trails) =
+      ({}, {}, [], []);
+  $self->generate($world, $altitude, $settlements, $trails, $step);
 
   # when documenting or debugging, do this before collecting lines
   if ($step > 0) {
@@ -2352,8 +2348,6 @@ sub generate_map {
   local $" = "-"; # list items separated by -
   my @lines;
   push(@lines, map { $_ . " " . $world->{$_} } sort keys %$world);
-  push(@lines, map { "@$_ canyon" } @$canyons);
-  push(@lines, map { "@$_ river" } @$rivers);
   push(@lines, map { "$_ trail" } @$trails);
   push(@lines, "include $contrib/gnomeyland.txt");
 
@@ -2365,6 +2359,149 @@ sub generate_map {
 	   my $n = int(25.5 * $_);
 	   qq{height$_ attributes fill="rgb($n,$n,$n)"};
 	 } (0 .. 10));
+    # visualize water flow
+    push(@lines, $self->arrows());
+  }
+
+  push(@lines, "# Seed: $seed");
+  push(@lines, "# Documentation: " . $url) if $url;
+  my $map = join("\n", @lines);
+  return $map;
+}
+
+package Schroeder::Archipelago;
+use Modern::Perl '2018';
+use Mojo::Base -base;
+use Role::Tiny::With;
+with 'Schroeder::Base';
+use List::Util qw'shuffle min max';
+
+has 'bottom' => 0;
+has 'top' => 10;
+has 'radius' => 5;
+has 'width' => 30;
+has 'height' => 10;
+has 'concentration' => 0.1;
+has 'eruptions' => 0.03;
+has 'world' => sub { { } };
+has 'altitude' => sub { {} };
+
+sub flat {
+  my $self = shift;
+  $log->debug("initializing altitude map");
+  # initialize the altitude map; this is required so that we have a list of
+  # legal hex coordinates somewhere
+  for my $y (1 .. $self->height) {
+    for my $x (1 .. $self->width) {
+      my $coordinates = coordinates($x, $y);
+      $self->altitude->{$coordinates} = 0;
+      $self->world->{$coordinates} = "height0";
+    }
+  }
+}
+
+sub ocean {
+  my $self = shift;
+  $log->debug("placing ocean and water");
+  for my $coordinates (sort keys %{$self->altitude}) {
+    if ($self->altitude->{$coordinates} <= $self->bottom) {
+      my $ocean = 1;
+      for my $i ($self->neighbors()) {
+	my ($x, $y) = $self->neighbor($coordinates, $i);
+	my $legal = $self->legal($x, $y);
+	my $other = coordinates($x, $y);
+	next if not $legal or $self->altitude->{$other} <= $self->bottom;
+	$ocean = 0;
+      }
+      $self->world->{$coordinates} = $ocean ? "ocean" : "water";
+    }
+  }
+}
+
+sub eruption {
+  my $self = shift;
+  my $cx = int $self->width * rand();
+  my $cy = int $self->height * (rand() + rand()) / 2;
+  $log->debug("eruption at " . $self->coordinates($cx, $cy));
+  my $top = 1 + int($self->top * $cx / $self->width);
+  $top-- if $top > 2 and rand() < 0.6;
+  for my $coordinates (keys %{$self->altitude}) {
+    my $d = $self->distance($self->xy($coordinates), $cx, $cy);
+    if ($d <= $top) {
+      my $h = $top - $d;
+      $self->altitude->{$coordinates} = $h if $h > $self->altitude->{$coordinates};
+      $self->world->{$coordinates} = "height" . $self->altitude->{$coordinates};
+    }
+  }
+}
+
+sub generate {
+  my $self = shift;
+  my $step = shift;
+  my @code = (sub { $self->flat() });
+  for (1 .. $self->width * $self->height * $self->eruptions) {
+    push(@code, sub { $self->eruption() });
+  }
+  push(@code, sub { $self->ocean() });
+
+  # $step 0 runs all the code; note that we can't simply cache those results
+  # because we need to start over with the same seed!
+  my $i = 1;
+  while (@code) {
+    shift(@code)->();
+    return if $step == $i++;
+  }
+}
+
+sub generate_map {
+  my $self = shift;
+  # If provided, the arguments override the defaults
+  $self->width(shift // $self->width);
+  $self->height(shift // $self->height);
+  $self->concentration(shift // $self->concentration);
+  $self->eruptions(shift // $self->eruptions);
+  $self->top(shift // $self->top);
+  $self->bottom(shift // $self->bottom);
+  my $seed = shift||time;
+  my $url = shift;
+  my $step = shift||0;
+
+  # For documentation purposes, I want to be able to set the pseudo-random
+  # number seed using srand and rely on rand to reproduce the same sequence of
+  # pseudo-random numbers for the same seed. The key point to remember is that
+  # the keys function will return keys in random order. So if we loop over the
+  # result of keys, we need to look at the code in the loop: If order is
+  # important, that wont do. We need to sort the keys. If we want the keys to be
+  # pseudo-shuffled, use shuffle sort keys.
+  srand($seed);
+
+  # keys for all hashes are coordinates such as "0101".
+  $self->generate($step);
+
+  # when documenting or debugging, do this before collecting lines
+  if ($step > 0) {
+    # add a height label at the very end
+    if ($step) {
+      for my $coordinates (keys %{$self->altitude}) {
+	$self->world->{$coordinates} .= ' "' . $self->altitude->{$coordinates} . '"';
+      }
+    }
+  }
+
+  local $" = "-"; # list items separated by -
+  my @lines;
+  push(@lines, map { $_ . " " . $self->world->{$_} } sort keys %{$self->world});
+  # push(@lines, map { "$_ trail" } @$trails);
+  push(@lines, "include $contrib/gnomeyland.txt");
+
+  # when documenting or debugging, add some more lines at the end
+  if ($step > 0) {
+    # visualize height
+    push(@lines,
+	 map {
+	   my $n = int(255 / $self->top * $_);
+	   qq{height$_ attributes fill="rgb($n,$n,$n)"};
+	 } (0 .. $self->top));
     # visualize water flow
     push(@lines, $self->arrows());
   }
@@ -3820,6 +3957,45 @@ sub island_map {
 get '/island/random' => sub {
   my $c = shift;
   my $map = island_map($c);
+  my $type = $c->param('type') // 'hex';
+  my $mapper;
+  if ($type eq 'hex') {
+    $mapper = Mapper::Hex->new();
+  } else {
+    $mapper = Mapper::Square->new();
+  }
+  my $svg = $mapper->initialize($map)->svg;
+  $c->render(text => $svg, format => 'svg');
+};
+
+sub archipelago_map {
+  my $c = shift;
+  # must be able to override this for the documentation
+  my $step = shift // $c->param('step');
+  # need to compute the seed here so that we can send along the URL
+  my $seed = $c->param('seed') || int(rand(1000000000));
+  my $url = $c->url_with('archipelagodocument')->query({seed => $seed})->to_abs;
+  my @params = ($c->param('width'),
+		$c->param('height'),
+		$c->param('concentration'),
+		$c->param('eruptions'),
+		$c->param('top'),
+		$c->param('bottom'),
+		$seed,
+		$url,
+		$step,
+      );
+  my $type = $c->param('type') // 'hex';
+  if ($type eq 'hex') {
+    return Schroeder::Archipelago->with_roles('Schroeder::Hex')->new()->generate_map(@params);
+  } else {
+    return Schroeder::Archipelago->with_roles('Schroeder::Square')->new()->generate_map(@params);
+  }
+}
+
+get '/archipelago/random' => sub {
+  my $c = shift;
+  my $map = archipelago_map($c);
   my $type = $c->param('type') // 'hex';
   my $mapper;
   if ($type eq 'hex') {
